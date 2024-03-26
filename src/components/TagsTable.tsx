9e5fa3598641ebfc4577e9854845945788db2ce9
Klaudia Kieryk <klaudiakieryk123@gmail.com>
2024-03-26 22:54:02 +0100

    Implement pagination and sorting table

--- src/components/TagsTable.tsx
+++ src/components/TagsTable.tsx
@@ -1,22 +1,57 @@
-import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material"
+import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Box } from "@mui/material"
 import { FC } from "react"
 import { TagType } from "../utils/TagType"
+import { SortType } from "../utils/SortType"
+import { OrderType } from "../utils/OrderType"
+import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
+import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
 
-const TagsTable: FC<{ tags: TagType[]}> = ({ tags }) => {
+const TagsTable: FC<{ tags: TagType[], sort: SortType, setSort: (sort: SortType) => void, order: OrderType, setOrder: (order: OrderType) => void}> = ({ tags, sort, setSort, order, setOrder }) => {
+
+  const handleSort = (sortBy: SortType) => {
+    if (sort === sortBy) {
+      const newOrder: OrderType = order === 'asc' ? 'desc' : 'asc'
+      setOrder(newOrder)
+    } else {
+      setSort(sortBy)
+      setOrder('asc')
+    }
+  }
+
+   const getSortIndicator = (sortBy: SortType) => {
+    if (sort === sortBy) {
+      return order === 'asc' ? <ArrowUpwardIcon/> : <ArrowDownwardIcon/>
+    }
+    return null
+  }
+
   return (
     <TableContainer component={Paper}>
       <Table>
         <TableHead>
           <TableRow>
-            <TableCell>Name</TableCell>
-            <TableCell>Amount</TableCell>
+            <TableCell 
+              sx={{
+                fontWeight: 600, 
+                width: '50%', 
+                '&:hover': {cursor: 'pointer', color: 'gray'}}} 
+              onClick={() => handleSort('name')}>
+                <Box sx={{display: 'flex', justifyContent: 'center'}}>Name {getSortIndicator('name')}</Box>
+            </TableCell>
+            <TableCell 
+              sx={{
+                fontWeight: 600, 
+                '&:hover': {cursor: 'pointer', color: 'gray'}}} 
+              onClick={() => handleSort('popular')}>
+                <Box sx={{display: 'flex', justifyContent: 'center'}}>Amount {getSortIndicator('popular')}</Box>
+            </TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
           { tags && tags.map((tag: TagType) => (
             <TableRow key={tag.name}>
-              <TableCell>{tag.name}</TableCell>
-              <TableCell>{tag.count}</TableCell>
+              <TableCell sx={{textAlign: 'center'}}>{tag.name}</TableCell>
+              <TableCell sx={{textAlign: 'center'}}>{tag.count}</TableCell>
             </TableRow>
           ))}
         </TableBody>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,20 +1,27 @@
-import { Box, Button, Typography, Paper, TextField } from "@mui/material"
+import { Box, Typography, Paper, TextField } from "@mui/material"
 import TagsTable from "../components/TagsTable"
 import { ChangeEvent, useEffect, useState } from "react"
 import { TagType } from "../utils/TagType"
 import axios from "axios"
+import { SortType } from "../utils/SortType"
+import { OrderType } from "../utils/OrderType"
+import NavigateBeforeIcon from '@mui/icons-material/NavigateBefore';
+import NavigateNextIcon from '@mui/icons-material/NavigateNext';
 
 const Home = () => {
 
     const BASE_URL: string = 'https://api.stackexchange.com/2.3/tags?site=stackoverflow';
-    const [tags, setTags] = useState<TagType[]>([]);
+
+    const [tags, setTags] = useState<TagType[]>([{name: 'js', count: 2}, {name: 'java', count: 10}]);
     const [tagsPerPage, setTagsPerPage] = useState<number>(5);
     const [page, setPage] = useState<number>(1);
     const [isNext, setIsNext] = useState<boolean>(false);
+    const [sort, setSort] = useState<SortType>('popular')
+    const [order, setOrder] = useState<OrderType>('desc')
 
     const fetchTags = async () => {
         try {
-            const result = await axios.get(`${BASE_URL}&pagesize=${tagsPerPage}&page=${page}`)
+            const result = await axios.get(`${BASE_URL}&pagesize=${tagsPerPage}&page=${page}&sort=${sort}&order=${order}`)
             setTags(result.data.items)
             setIsNext(result.data.has_more)
         } catch (err) {
@@ -37,26 +44,34 @@ const Home = () => {
 
     useEffect(() => {
         fetchTags()
-    },[page, tagsPerPage])
+    },[page, tagsPerPage, sort, order])
 
 
   return (
     <>
         <Typography variant="h1" sx={{paddingBottom: 3}}>StackOverflow Tags</Typography>
-        <Paper sx={{marginBottom: 2, padding: 2, display: 'flex', justifyContent: 'space-between', alignItems:'center'}}>
+        <Paper 
+            sx={{
+                marginBottom: 2,
+                padding: 2,
+                display: 'flex',
+                justifyContent: 'space-between',
+                alignItems:'center'}}>
             <TextField 
                 type='number' 
                 value={tagsPerPage || ''} 
                 onChange={handlePerPageChange} 
-                inputProps={{min: 1}}
-                label="how many tags?"/>
-            <Box>
-                { page > 1 && <Button onClick={handlePreviousPage}>previos</Button>}
-                { isNext && <Button onClick={handleNextPage}>next</Button>}
+                inputProps={{min: 1, max:100}}
+                label="how many tags?"
+                sx={{width: '30%'}}
+            />
+            <Box display='flex' gap={2}>
+                { page > 1 && <NavigateBeforeIcon onClick={handlePreviousPage} sx={{'&:hover': {cursor: 'pointer', color: 'gray'}}}/>}
+                <Typography>{page}</Typography>
+                { isNext && page < 25 && <NavigateNextIcon onClick={handleNextPage} sx={{'&:hover': {cursor: 'pointer', color: 'gray'}}}/>}
             </Box>
-
         </Paper>
-        <TagsTable tags={tags}/>
+        <TagsTable tags={tags} sort={sort} setSort={setSort} order={order} setOrder={setOrder}/>
     </>
   )
 }
